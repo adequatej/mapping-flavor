675ea3a6f0e64157d43f1fb2f5e4ce8e3ca33382
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-06-01 19:52:29 +0800

    fix: fixed to use CommonJS format that works w/ next.js and got rid of tailwind verson conflicts to use proper PostCSS configuration format and unintalled redundant dep

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
-    localeDetection: true,
-  },
   images: {
     remotePatterns: [
       {
