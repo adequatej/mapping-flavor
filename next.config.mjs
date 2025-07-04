ab5e8578f3176ae85912a2f9e7442f7f44222c9c
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-07-04 10:59:17 -0400

    updated project configuration and environment setup

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
+
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
